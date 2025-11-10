6614713042a5a50b796ba4680d2fa0081bbaac6d
Eritque arcus <eritque-arcus@ikuyo.dev>
2025-11-09 22:01:39 -0600

    chore: cleanup

--- main.ts
+++ main.ts
@@ -29,9 +29,13 @@ export default class PseudocodePlugin extends Plugin {
 	): Promise<any> {
 		const blockDiv = el.createDiv({ cls: "pseudocode-block" });
 		const blockWidth = this.settings.blockSize;
-		blockDiv.style.width = `${blockWidth}em`;;
+		blockDiv.style.width = `${blockWidth}em`;
 		blockDiv.addEventListener("click", (event) => {
-			((event.target as HTMLElement).closest('.pseudocode-block')!.parentElement!.parentElement!.querySelector('.edit-block-button') as HTMLElement)!.click();
+			((event.target as HTMLElement)
+                .closest('.pseudocode-block')!
+                .parentElement!.parentElement!
+                .querySelector('.edit-block-button') as HTMLElement)!
+                .click();
 		});
 
 		// Extract inline macros
